19ac153e996c1b9347488df425cd7afdedbc8ac6
harryisgood <hairypyles@gmail.com>
2022-10-12 10:18:23 +0100

    Updated web3.storage uploads
    Was using python library but that didn't work
    changed to using subprocess library in python to fix issues

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,29 +3,34 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import boto3
 import os
-from s3_functions import list_files, upload_file, show_image
+from s3_functions import upload_file, show_songs
 import sys
 import time
-
+import subprocess
 from pydub import AudioSegment
 from pydub.playback import play
 import io
+import sys
+import time
+sys.path.append('/path/to/ffmpeg')
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER='static/'
+UPLOAD_FOLDER='flask-app/static/'
 
 @app.route("/")
 def home(): 
-    contents = show_songs()
-    client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
+    contents = ['one.mp3','two.mp3', 'file_example_MP3_1MG.mp3', 'three.mp3'] #show_songs()
+    #client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
     songs = [] 
     for i in contents:
-    	
-    	data = client.cat(i)
-		song = AudioSegment.from_file(io.BytesIO(data), format="mp3")
-		songs.append(song)
-
+        #data = client.cat(i)import subprocess
+        #result = subprocess.run(['ipfs', 'cat', i], stdout=subprocess.PIPE)
+        #print(result, file=sys.stderr)
+        #data = result.stdout
+        #song = AudioSegment.from_file(io.BytesIO(data), format="mp3")
+        songs.append(i)
+        
     return render_template('index.html', songs=songs)
 
 @app.route("/upload", methods=['POST'])
@@ -34,7 +39,7 @@ def upload():
         f = request.files['file']
         f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
         time.sleep(3)
-        upload_file(UPLOAD_FOLDER + f"/{f.filename}", BUCKET)
+        upload_file(UPLOAD_FOLDER + f"/{f.filename}")
         return redirect("/")
 
 if __name__ == '__main__':
--- flask-app/s3_functions.py
+++ flask-app/s3_functions.py
@@ -1,25 +1,35 @@
-import boto3
-import ipfshttpclient
+import subprocess
 import sys
+import time
 
-def upload_file(file_name, bucket):
+def upload_file(file_name):
     object_name = file_name
     object_name = object_name.split("/")
 
     obj = object_name[-2] +'/'+object_name[-1]
 
-    client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
-    res = client.add(file_name)
-
-    f=open('hashes.txt')
+    result = subprocess.run(['ipfs', 'add', file_name], stdout=subprocess.PIPE)
+    url = subprocess.run(['w3', 'put', file_name], stdout=subprocess.PIPE)
+    url = url.stdout.decode('utf-8')
+    url = url.split('\n')[1]
+    url = url[1:]
+    print('THIs is URL:  '+ str(url), file=sys.stderr)
+    res = result.stdout.decode("utf-8")
+    print(res.split(' ')[1], file=sys.stderr)
+    res = res.split(' ')[1]
+    time.sleep(5)
+    f=open('flask-app/hashes.txt', 'a')
     f.write(res)
+    f.write('\n')
+    f.write(url)
+    f.write('\n')
     f.close()
 
     return res
 
 def show_songs():
     public_urls = []
-    with open('hashes.txt') as f:
+    with open('flask-app/hashes.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
             public_urls.append(line.strip())
